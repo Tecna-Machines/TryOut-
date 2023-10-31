592ed53f443c6998797e5e4719d6ff30e959c36b
Gabriel <matiascoria427@gmail.com>
2023-10-31 16:01:38 -0300

    modificaciones minimas en la consola

--- ConsolaTest/Program.cs
+++ ConsolaTest/Program.cs
@@ -4,31 +4,40 @@ using Domain.Entities;
 using Infrastructure.Persistence;
 using Infrastructure.Repositories;
 
-//repositorio
-IMaquinaRepository repository = new MaquinaRepository(new TryOutContext());
-
-String? NumMaquina, Nombre , Descripcion;
-
-Console.Write("ingresa el numero de maquina: ");
-NumMaquina = Console.ReadLine();
-
-Console.Write("ingresa el nombre de la maquina: ");
-Nombre = Console.ReadLine();
-
-Console.Write("ingresa la descripcion: ");
-Descripcion = Console.ReadLine();
-
-Maquina nuevaMaquina = new Maquina
+while (true)
 {
-    NumMaquina = NumMaquina,
-    Nombre = Nombre,
-    Descripcion = Descripcion,
-    FechaCreacion = DateTime.Now
-};
+    //repositorio
+    IMaquinaRepository repository = new MaquinaRepository(new TryOutContext());
+
+    String? NumMaquina, Nombre, Descripcion;
+
+    Console.Write("ingresa el numero de maquina: ");
+    NumMaquina = Console.ReadLine();
+
+    Console.Write("ingresa el nombre de la maquina: ");
+    Nombre = Console.ReadLine();
+
+    Console.Write("ingresa la descripcion: ");
+    Descripcion = Console.ReadLine();
+
+    Maquina nuevaMaquina = new Maquina
+    {
+        NumMaquina = NumMaquina,
+        Nombre = Nombre,
+        Descripcion = Descripcion,
+        FechaCreacion = DateTime.Now
+    };
 
 
-repository.InsertMaquina(nuevaMaquina);
-repository.Save();
+    Maquina creada = await repository.InsertMaquina(nuevaMaquina);
+    repository.Save();
+
+    Console.WriteLine("---------------------------------");
+    Console.WriteLine("se creo la siguiente maquina: ");
+    Console.WriteLine("Modelo: M-" + creada.NumMaquina);
+    Console.WriteLine("Nombre: " + creada.Nombre);
+    Console.WriteLine("Descripcio: " + creada.Descripcion);
+}
 
 
 
